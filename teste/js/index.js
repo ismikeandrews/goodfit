19724f866b2d6cae85726987ee19a5736e60be7e
Cyntia Almeida <cyntia.almeida@fiap.com.br>
2019-09-27 15:01:58 -0300

    #

--- teste/js/index.js
+++ teste/js/index.js
@@ -1,22 +1,21 @@
-function funcionalidades() {
-  let modal = document.querySelector(".modal-content");
-  let openModal = document.querySelector(".menu-item-login");
-  let closeModal = document.querySelector(".modal-close-link");
-  const bg = document.querySelector(".everything");
 
-  openModal.onclick = function() {
-    modal.style.display = "block";
-    bg.classList.add('bg-dark');
-  };
+// let modal = document.querySelector(".modal-content");
+// let openModal = document.querySelector(".menu-item-login");
+// let closeModal = document.querySelector(".modal-close-link");
+// const bg = document.querySelector(".everything");
 
-  closeModal.onclick = function() {
-    modal.style.display = "none";
-    bg.classList.remove('bg-dark');
-  };
+// openModal.onclick = function() {
+//   modal.style.display = "block";
+//   bg.classList.add('bg-dark');
+// };
 
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-}
+// closeModal.onclick = function() {
+//   modal.style.display = "none";
+//   bg.classList.remove('bg-dark');
+// };
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
