7a35b0bca9f5998504e4e7c50ffa0c90be63159a
Cyntia Almeida <cyntia.almeida@fiap.com.br>
2019-10-07 16:29:48 -0300

    Curriculo | Etapa 2 - Layout e Icones

--- aGoodFit/resources/views/curriculo/curriculo-etapa2.blade.php
+++ aGoodFit/resources/views/curriculo/curriculo-etapa2.blade.php
@@ -5,3 +5,41 @@
     <div class="curriculo-desc">
         Explicar o que é essa sessão
     </div>
+
+    <div class="curriculo-etapa2">
+        <div class="curriculo-etapa2-content">
+            <div class="curriculo-etapa2-content-item">
+                <img src="{{asset('images/icones/habilidades/comunicativo.png')}}" alt="Habilidades - Comunicativo" class="curriculo-habilidades-icon">
+            </div>
+            <div class="curriculo-etapa2-content-item">
+                <img src="{{asset('images/icones/habilidades/criativo.png')}}" alt="Habilidades - Criativo" class="curriculo-habilidades-icon">
+            </div>
+        </div>
+
+        <div class="curriculo-etapa2-content">
+            <div class="curriculo-etapa2-content-item">
+                <img src="{{asset('images/icones/habilidades/tempo.png')}}" alt="Habilidades - Gerenciamento de tempo" class="curriculo-habilidades-icon">
+            </div>
+            <div class="curriculo-etapa2-content-item">
+                <img src="{{asset('images/icones/habilidades/relacionamento.png')}}" alt="Habilidades - Bom relacionamento" class="curriculo-habilidades-icon">
+            </div>
+        </div>
+        
+        <div class="curriculo-etapa2-content">
+            <div class="curriculo-etapa2-content-item">
+                <img src="{{asset('images/icones/habilidades/matematica.png')}}" alt="Habilidades - Bom com números" class="curriculo-habilidades-icon">
+            </div>
+            <div class="curriculo-etapa2-content-item">
+                <img src="{{asset('images/icones/habilidades/escrita.png')}}" alt="Habilidades - Escreve bem" class="curriculo-habilidades-icon">
+            </div>
+        </div>
+        
+        <div class="curriculo-etapa2-content">
+            <div class="curriculo-etapa2-content-item">
+                <img src="{{asset('images/icones/habilidades/instrumento.png')}}" alt="Habilidades - Toca algum instrumento" class="curriculo-habilidades-icon">
+            </div>
+            <div class="curriculo-etapa2-content-item">
+                <img src="{{asset('images/icones/habilidades/pintura.png')}}" alt="Habilidades - Desenha bem" class="curriculo-habilidades-icon">
+            </div>
+        </div>
+    </div>
