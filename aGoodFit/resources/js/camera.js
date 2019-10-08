b6986f12c1b597e2b2cff453ce65b398c8ad080d
Cyntia Almeida <cyntia.almeida@fiap.com.br>
2019-10-08 17:40:06 -0300

    tentando colocar instascan e resolvendo conflitos

---
+++ aGoodFit/resources/js/camera.js
@@ -0,0 +1,7 @@
+// const camera = document.querySelector('#camera');
+
+// alert('eu aqui')
+// camera.addEventListener('click', function() {
+//     alert('eu de novo')
+// 	context.drawImage(video, 0, 0, 640, 480);
+// });
