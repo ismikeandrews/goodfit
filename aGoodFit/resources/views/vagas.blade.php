ccd2e827cc77acd910965ad2f15c9aa3c7608518
Cyntia Almeida <cyntia.almeida@fiap.com.br>
2019-10-09 19:09:35 -0300

    Vagas | Alteração layout do header

--- aGoodFit/resources/views/vagas.blade.php
+++ aGoodFit/resources/views/vagas.blade.php
@@ -4,36 +4,36 @@
 
 <section class="vagas">
 	<div class="vagas-empresa">
-		<div class="vagas-empresa-logo vagas-empresa-logo-mc">
-			<img src="{{asset('images/componentes/empresa-mc.png')}}" alt="Vagas - MC Donalds" class="vagas-empresa-logo-img">
+		<div class="vagas-empresa-logo">
+			<img src="{{asset('images/componentes/empresa-outback.png')}}" alt="Vagas - Outback" class="vagas-empresa-logo-img">
 		</div>
+	</div>
 
-		<div class="vagas-btn">
-			<div class="vagas-btn-item">
-				<img src="{{asset('images/componentes/vagas-x.svg')}}" alt="Recusar vaga" class="vagas-btn-icon">
-			</div>
-			<div class="vagas-btn-item">
-				<img src="{{asset('images/componentes/vagas-check.svg')}}" alt="Aceitar vaga" class="vagas-btn-icon">
-			</div>
+	<div class="vagas-btn">
+		<div class="vagas-btn-item">
+			<img src="{{asset('images/componentes/vagas-x.svg')}}" alt="Recusar vaga" class="vagas-btn-icon">
+		</div>
+		<div class="vagas-btn-item">
+			<img src="{{asset('images/componentes/vagas-check.svg')}}" alt="Aceitar vaga" class="vagas-btn-icon">
 		</div>
 	</div>
 		
 	<div class="container container-vagas">
 		<div class="vagas-content">
 			<h1 class="vagas-content-title">
-				Analista de Performance (Google Adwords)
+				Cozinheiro
 			</h1>
 
 			<div class="vagas-content-desc">
 				<img src="{{asset('images/icones/empresa.png')}}" alt="Nome da empresa" class="vagas-content-desc-icon">
 				<p class="vagas-content-desc-nome">
-					Mc Donald's Brasil
+					Outback Steakhouse Restaurantes Brasil S.a.
 				</p>
 			</div>
 			<div class="vagas-content-desc">
 				<img src="{{asset('images/icones/local.png')}}" alt="Endereço da empresa" class="vagas-content-desc-icon">
 				<p class="vagas-content-desc-local">
-					Rua Bento de Andrade, 660, Jardim Paulista - São Paulo, SP
+					Rua Engenheiro Camilo Oliveti 295 loja B6A - Vila Itapegica, Guarulhos
 				</p>
 			</div>
 
@@ -48,11 +48,13 @@
 				</div>
 			</div>
 
-			<div class="about">
-				<h3 class="about-title">Descrição</h3>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			<div class="vagas-content-sobre">
+				<h3 class="vagas-content-sobre-title">Descrição</h3>
+				<p class="vagas-content-sobre-desc">
+					Auxiliar no pré-preparodos alimentos solicitados pelos clientes, seguindo os procedimentosoperacionais padronizados determinados pelo Outback, mantendo sempre aconfidencialidade dos ingredientes e procedimentos envolvidos durante oprocesso, agilizando a finalização dos produtos garantindo a qualidade Outback.
+				</p>
 
-				<h3 class="about-title">Requisitos</h3>
+				<h3 class="vagas-content-sobre-title">Requisitos</h3>
 					<ul>
 						<li>- Certificado no Google Ads avançado</li>
 						<li>- Certificado no Google Analytics avançado</li>
@@ -60,13 +62,13 @@
 						<li>- Perfil analítico</li>
 					</ul>
 
-				<h3 class="about-title">Desejável</h3>
+				<h3 class="vagas-content-sobre-title">Desejável</h3>
 					<ul>
 						<li>- Conhecimento de estratégias de Marketing</li>
 						<li>- Ensino Superior completo ou cursando</li>
 					</ul>
 
-				<h3 class="about-title">Benefícios</h3>
+				<h3 class="vagas-content-sobre-title">Benefícios</h3>
 				<ul>
 					<li>- VT</li>
 					<li>- VR / VA</li>
