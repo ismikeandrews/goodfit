b71faef1f7a81eb2ec16fef0a1a51fc7b9fdc0a1
Jonatas Alves De Oliveira <jonadol@br.ibm.com>
2019-10-09 22:37:41 -0300

    inserir descrição dos inputs

--- aGoodFit/resources/views/configPerfil.blade.php
+++ aGoodFit/resources/views/configPerfil.blade.php
@@ -19,17 +19,18 @@
 			<input id='selecao-arquivo' type="file" name="foto" class="content-perfil-input-foto" accept=".jpg, .jpeg, .png">
 		</div>
 
-		<div class="content-perfil-image">
+		<div class="content-perfil-desc">
+			Nome
 			<input class="form-input-item form-input-item-perfil" type="text" name="name" value="{{$candidato->nomeCandidato}}">
-			
+			CPF
 			<input class="form-input-item form-input-item-perfil" type="number" name="name" value="{{$candidato->cpfCandidato}}">
-			
+			RG
 			<input class="form-input-item form-input-item-perfil" type="number" name="name" value="{{$candidato->rgCandidato}}">
-			
+			Data de nascimento
 			<input class="form-input-item form-input-item-perfil" type="text" name="name" value="{{$candidato->dataNascimentoCandidato}}">
-			
+			Login
 			<input class="form-input-item form-input-item-perfil" type="text" name="name" value="{{$usuario->loginUsuario}}">
-			
+			E-mail
 			<input class="form-input-item form-input-item-perfil" type="text" name="name" value="{{$usuario->email}}">
 
 			<input class="btn btn-perfil-salvar" type="submit" value="Salvar">
