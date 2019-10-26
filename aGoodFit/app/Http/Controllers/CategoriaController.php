e7a3a115e9c2f94aa0feba91c170da17ff62cef2
Vanessa Amaral Marques <vanessa2011164@hotmail.com>
2019-10-26 13:35:55 -0300

    Adicionadas funções para pegar categorias

--- aGoodFit/app/Http/Controllers/CategoriaController.php
+++ aGoodFit/app/Http/Controllers/CategoriaController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CategoriaController extends Controller
 {
@@ -17,5 +19,34 @@ class CategoriaController extends Controller
     	]);
 
     	return view('cadastroCategoria')->with('ok', $categoria->save());
+    }
+
+    /**
+    * Função para pegar todos as categorias
+    * 
+    *
+    * @author Vanessa Amaral Marques
+    **/
+    public function getCategorias(){
+    	return DB::table('tbCategoria')
+	      ->select('tbCategoria.codCategoria', 'tbCategoria.nomeCategoria', 'tbCategoria.imagemCategoria')
+	      ->orderBy('tbCategoria.nomeCategoria')
+	      ->get();
+    }
+
+    /**
+    * Função para pegar todos as categorias de um curriculo
+    * 
+    * @param $codCurriculo codigo do curriculo
+    *
+    * @author Vanessa Amaral Marques
+    **/
+    public function getCategoriasByCurriculo(int $codCurriculo){
+    	return DB::table('tbCategoria')
+	      ->select('tbCategoria.codCategoria')
+	      ->join('tbCargoCurriculo', 'tbCategoria.codCategoria', '=', 'tbCargoCurriculo.codCategoria')
+	      ->where('tbCargoCurriculo.codCurriculo', '=', $codCurriculo)
+	      ->orderBy('tbCategoria.nomeCategoria')
+	      ->get();
     }
 }
