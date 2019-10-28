7095c16f8c9bb69cb5893af8db932478a9eeefc2
Michael Andrews <michaelcl98@hotmail.com>
2019-10-28 01:34:14 -0300

    Ajuste na pagina de vagas

--- aGoodFit/resources/views/vagas.blade.php
+++ aGoodFit/resources/views/vagas.blade.php
@@ -28,7 +28,7 @@
 	<section class="container-vagas vagas">
 		<div class="vagas-empresa">
 			<div class="vagas-empresa-logo">
-				<img src="images/empresas/{{$vaga->usuario->fotoUsuario}}" alt="Vagas - {{$vaga->empresa->nomeFantasiaEmpresa}}" class="vagas-empresa-logo-img">
+				<img src="images/empresas/{{$vaga->usuario->fotoUsuario}}" alt="Vagas - {{$vaga->nomeFantasiaEmpresa}}" class="vagas-empresa-logo-img">
 			</div>
 		</div>
 
