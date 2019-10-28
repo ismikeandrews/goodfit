0ee6e000dd2c55c306782220fe871eb1df0a82e5
Michael Andrews <michaelcl98@hotmail.com>
2019-10-28 01:34:45 -0300

    novo metodo para criar uma nova empresa

--- aGoodFit/app/Http/Controllers/EmpresaController.php
+++ aGoodFit/app/Http/Controllers/EmpresaController.php
@@ -7,6 +7,27 @@ use Illuminate\Support\Facades\DB;
 
 class EmpresaController extends Controller
 {
+  /**
+  * Função para cadastrar uma Empresa
+  *
+  * @param array $data array com os dados do formulario
+  * @param int $codUsuario id do usuario recem cadastrado
+  *
+  * @author Michael Andrews
+  **/
+  public function novaEmpresa(array $data, int $codUsuario){
+    $cnpj = $data['cnpj'];
+    $regex = '/[^0-9]/';
+    $cnpj = preg_replace($regex, '', $cnpj);
+
+   Candidato::create([
+     'nomeFantasiaEmpresa' => $data['nomeFantasiaEmpresa'],
+     'razaoSocialEmpresa' => $cnpj,
+     'cnpjEmpresa' => $data['cnpjEmpresa'],
+     'codUsuario' => $codUsuario
+   ]);
+  }
+
 	/**
     * Função para pegar empresa por codigo
     *
