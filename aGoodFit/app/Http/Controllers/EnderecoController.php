6657f7bc92079f9ce979e7e4744d03f59c7d6c15
Michael Andrews <michaelcl98@hotmail.com>
2019-11-01 01:44:28 -0300

    Função para cadastrar um endereco

--- aGoodFit/app/Http/Controllers/EnderecoController.php
+++ aGoodFit/app/Http/Controllers/EnderecoController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Candidato;
-use Auth;
+use App\Endereco;
 use Illuminate\Support\Facades\DB;
 
 class EnderecoController extends Controller
 {
-  public function index(){
+  public function novoEndereço(array $data){
+    $cep = $data['cep'];
+    $regex = '/[^0-9]/';
+    $cep = preg_replace($regex, '', $cep);
 
-    return view('endereco.endereco-candidato');
+    $endereco = Endereco::create([
+      'cepEndereco' => $cep,
+      'logradouroEndereco' => $data['logradouro'],
+      'numeroEndereco' => $data['bairro'],
+      'complementoEndereco' => $data['complemento'],
+      'bairroEndereco' => $data['bairro'],
+      'zonaEndereco' => $data['zona'],
+      'cidadeEndereco' => $data['cidade'],
+      'estadoEndereco' => $data['estado'],
+    ]);
+    return $endereco->codEndereco;
   }
 }
