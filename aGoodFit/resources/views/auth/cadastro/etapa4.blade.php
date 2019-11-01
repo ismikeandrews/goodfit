ccf4c16f751326127a87d8e2e7bd0d107e6ea5bb
Michael Andrews <michaelcl98@hotmail.com>
2019-11-01 01:45:52 -0300

    cadastro do usuario com o novo formulario

--- aGoodFit/resources/views/auth/cadastro/etapa4.blade.php
+++ aGoodFit/resources/views/auth/cadastro/etapa4.blade.php
@@ -18,7 +18,7 @@
 
         <div class="content-perfil-desc-item">
           Logradouro
-          <input class="form-input-item form-input-item-perfil @error('logradouro') is-invalid @enderror" id="logradouro" type="logradouro" name="logradouro" value="{{ old('logradouro') }}"  autocomplete="logradouro" autofocus>
+          <input class="form-input-item form-input-item-perfil @error('logradouro') is-invalid @enderror" id="logradouro" type="text" name="logradouro" value="{{ old('logradouro') }}"  autocomplete="logradouro" autofocus>
           @error('logradouro')
           <span class="erro" role="alert">
             <strong>{{ $message }}</strong>
@@ -28,7 +28,7 @@
 
         <div class="content-perfil-desc-item">
           Bairro
-          <input class="form-input-item form-input-item-perfil @error('bairro') is-invalid @enderror" type="bairro" id="bairro" name="bairro" value="{{ old('bairro') }}" autocomplete="bairro" autofocus>
+          <input class="form-input-item form-input-item-perfil @error('bairro') is-invalid @enderror" type="text" id="bairro" name="bairro" value="{{ old('bairro') }}" autocomplete="bairro" autofocus>
           @error('bairro')
           <span class="erro" role="alert">
             <strong>{{ $message }}</strong>
@@ -38,7 +38,7 @@
 
         <div class="content-perfil-desc-item">
           Cidade
-          <input class="form-input-item form-input-item-perfil @error('cidade') is-invalid @enderror" type="cidade" id="cidade" name="cidade" value="{{ old('cidade') }}" autocomplete="cidade" autofocus>
+          <input class="form-input-item form-input-item-perfil @error('cidade') is-invalid @enderror" type="text" id="cidade" name="cidade" value="{{ old('cidade') }}" autocomplete="cidade" autofocus>
           @error('cidade')
           <span class="erro" role="alert">
             <strong>{{ $message }}</strong>
@@ -49,7 +49,7 @@
         <div class="content-perfil-desc-box">
           <div class="content-perfil-desc-item content-perfil-desc-box-item">
             Estado
-            <input class="form-input-item form-input-item-perfil @error('estado') is-invalid @enderror" id="estado" type="estado" name="estado" value="{{ old('estado') }}"  autocomplete="estado" autofocus>
+            <input class="form-input-item form-input-item-perfil @error('estado') is-invalid @enderror" id="estado" type="text" name="estado" value="{{ old('estado') }}"  autocomplete="estado" autofocus>
             @error('estado')
             <span class="erro" role="alert">
               <strong>{{ $message }}</strong>
@@ -59,7 +59,7 @@
 
           <div class="content-perfil-desc-item content-perfil-desc-box-item">
             Zona
-            <input class="form-input-item form-input-item-perfil @error('zona') is-invalid @enderror" type="zona" id="zona" name="zona" value="{{ old('zona') }}" autocomplete="zona" autofocus>
+            <input class="form-input-item form-input-item-perfil @error('zona') is-invalid @enderror" type="text" id="zona" name="zona" value="{{ old('zona') }}" autocomplete="zona" autofocus>
             @error('zona')
             <span class="erro" role="alert">
               <strong>{{ $message }}</strong>
@@ -71,7 +71,7 @@
         <div class="content-perfil-desc-box">
           <div class="content-perfil-desc-item content-perfil-desc-box-item">
             Número
-            <input class="form-input-item form-input-item-perfil @error('numero') is-invalid @enderror" id="numero" type="numero" name="numero" value="{{ old('numero') }}"  autocomplete="numero" autofocus>
+            <input class="form-input-item form-input-item-perfil @error('numero') is-invalid @enderror" id="numero" type="text" name="numero" value="{{ old('numero') }}"  autocomplete="numero" autofocus>
             @error('numero')
             <span class="erro" role="alert">
               <strong>{{ $message }}</strong>
@@ -81,7 +81,7 @@
 
           <div class="content-perfil-desc-item content-perfil-desc-box-item">
             Complemento
-            <input class="form-input-item form-input-item-perfil @error('complemento') is-invalid @enderror" type="complemento" id="complemento" name="complemento" value="{{ old('complemento') }}" autocomplete="complemento" autofocus>
+            <input class="form-input-item form-input-item-perfil @error('complemento') is-invalid @enderror" type="text" id="complemento" name="complemento" value="{{ old('complemento') }}" autocomplete="complemento" autofocus>
             @error('complemento')
             <span class="erro" role="alert">
               <strong>{{ $message }}</strong>
