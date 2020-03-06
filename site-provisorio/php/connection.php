6c4de370f235799d941039675d0faa269d3243db
Michael Andrews <michaelcl98@hotmail.com>
2020-03-05 23:45:50 -0300

    Cleaning branch

--- site-provisorio/php/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$serverName = "localhost";
-$username = "id11437989_root";
-$password  = "amoSEiSimEpe";
-$dbName = "id11437989_agoodfit";
-
-$con = mysqli_connect($serverName, $username, $password, $dbName);
--- site-provisorio/php/insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include_once 'connection.php';
-
-  $email = $_POST['email'];
-
-  $sql = "INSERT INTO email(email) VALUES('$email');";
-  mysqli_query($con, $sql);
-
-  header("Location: ../index.php?cadastro=success");
