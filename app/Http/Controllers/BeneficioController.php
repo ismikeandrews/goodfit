7056cc5508fc8ed5565b9a088814a96d2c788732
vanessa <vanessa2011164@hotmail.com>
2020-05-05 23:47:58 -0300

    Controller de beneficio para API

--- app/Http/Controllers/BeneficioController.php
+++ app/Http/Controllers/BeneficioController.php
@@ -2,38 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Beneficio;
 
 class BeneficioController extends Controller
 {
-	/**
-    * Função para pegar todos os beneficios de uma vaga
-    * 
-    * @param $codVaga codigo da vaga
-    *
-    * @author Vanessa Amaral Marques
-    **/
+    /**
+     * Função para pegar todos os beneficios de uma vaga
+     *
+     * @param int $codVaga
+     * @return JsonResponse
+     * @author Vanessa Amaral Marques
+     **/
     public function getBeneficioByVaga(int $codVaga){
-    	return DB::table('tbBeneficio')
+    	$beneficios =  DB::table('tbBeneficio')
         ->select('tbBeneficio.nomeBeneficio')
         ->where('tbBeneficio.codVaga', '=', $codVaga)
         ->orderBy('tbBeneficio.nomeBeneficio', 'ASC')
         ->get();
+
+    	return response()->json($beneficios, 200);
     }
-    
+
     /**
      * Função para cadastrar um novo beneficios de uma vaga
      *
-     * @param $data nomeBenficio, $codVaga codigo da vaga
-     *
+     * @param string $nome
+     * @param int $codVaga
+     * @return JsonResponse
      * @author Michael Andrews
-     **/
-    public function novoBeneficio($data, $codVaga){
-        Beneficio::create([
-            'nomeBeneficio' => $data,
-            'codVaga' => $codVaga,
-        ]);
+     * @author Vanessa Amaral Marques
+     */
+    public function novoBeneficio(string $nome, int $codVaga){
+        try {
+            $beneficio = Beneficio::create([
+                'nomeBeneficio' => $nome,
+                'codVaga'       => $codVaga,
+            ]);
+
+            if ( ! $beneficio->save() ) {
+                throw new \Exception('Erro ao tentar cadastrar benefício');
+            }
+        } catch ( \Exception $e ) {
+            return response()->json($e->getMessage(), 503);
+        }
     }
 }
