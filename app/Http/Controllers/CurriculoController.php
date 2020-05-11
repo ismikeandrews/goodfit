daa5db5ccaeed4daa7ba90eae7da2f2c2311922b
vanmrqs <vanessa2011164@hotmail.com>
2020-05-11 11:44:36 -0300

    Atualizados controllers de curriculo e empresa para API

--- app/Http/Controllers/CurriculoController.php
+++ app/Http/Controllers/CurriculoController.php
@@ -4,22 +4,30 @@ namespace App\Http\Controllers;
 
 use Auth;
 use App\Curriculo;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 
 class CurriculoController extends Controller
 {
 
-  public $adicionalController;
-  public $cargoCurriculoController;
-  public $categoriaController;
-  public $habilidadeController;
-  public $tipoAdicionalController;
+  private $usuario;
+  private $candidato;
 
-  public function __construct()
-  {
+  private $adicionalController;
+  private $cargoCurriculoController;
+  private $categoriaController;
+  private $habilidadeController;
+  private $tipoAdicionalController;
+
+  public function __construct() {
+    $this->usuario   = Auth::user();
+    $this->candidato = DB::table('tbCandidato')->where('codUsuario', $this->usuario->codUsuario)->first();
 
     //Controllers
     $this->adicionalController      = new adicionalController;
@@ -29,14 +37,9 @@ class CurriculoController extends Controller
     $this->tipoAdicionalController  = new TipoAdicionalController;
   }
 
-
   public function index(){
-    $usuario   = Auth::user();
-    $candidato = DB::table('tbCandidato')
-    ->where('codUsuario', $usuario->codUsuario)->first();
-
     $curriculo = DB::table('tbCurriculo')
-    ->where('codCandidato', $candidato->codCandidato)->first();
+    ->where('codCandidato', $this->candidato->codCandidato)->first();
 
     $habilidades         = $this->habilidadeController->getHabilidades();
     $categorias          = $this->categoriaController->getCategorias();
@@ -51,123 +54,114 @@ class CurriculoController extends Controller
     ];
 
     if ($curriculo) {
-      $candidato->habilidades   = $this->habilidadeController->getHabilidadesByCurriculo($curriculo->codCurriculo);
-      $candidato->categorias    = $this->categoriaController->getCategoriasByCurriculo($curriculo->codCurriculo);
-      $curriculo->escolaridade  = $this->adicionalController->getAdicionalByNomeTipoAndCurriculo('Escolaridade', $curriculo->codCurriculo);
-
-      $curriculo->alfabetizacao = $this->adicionalController->getAdicionalByNomeTipoAndCurriculo('Alfabetização', $curriculo->codCurriculo);
+      $this->candidato->habilidades   = $this->habilidadeController->getHabilidadesByCurriculo($curriculo->codCurriculo);
+      $this->candidato->categorias    = $this->categoriaController->getCategoriasByCurriculo($curriculo->codCurriculo);
+      $this->curriculo->escolaridade  = $this->adicionalController->getAdicionalByNomeTipoAndCurriculo('Escolaridade', $curriculo->codCurriculo);
+      $this->curriculo->alfabetizacao = $this->adicionalController->getAdicionalByNomeTipoAndCurriculo('Alfabetização', $curriculo->codCurriculo);
 
       $dados['curriculo'] = $curriculo;
-      $dados['candidato'] = $candidato;
-
-      return view('curriculo.configCurriculo', $dados);
+      $dados['candidato'] = $this->candidato;
     }
 
-    return view('curriculo.curriculo', $dados);
+    return response()->json($dados);
   }
 
   /**
-    * Função para cadastro do currículo
-    *
-    * @param $request dados do formulário
-    *
-    * @author Michael Andrews
-    **/
+  * Função para cadastro do currículo
+  *
+  * @param Request $request dados do formulário
+  * @return JsonResponse
+  * @throws ValidationException
+  * @author Michael Andrews
+  * @author Vanessa Amaral Marques
+  */
   public function novoCurriculo(Request $request){
-    $usuario = Auth::user();
-    $candidato = DB::table('tbCandidato')->where('codUsuario', $usuario->codUsuario)->first();
-
     $this->validate($request, [
       'habilidades' => 'required|array',
       'categorias' => 'required|array',
       'descricaoCurriculo' => 'string|nullable',
     ]);
 
-    $curriculo = Curriculo::create([
-      'codCandidato' => $candidato->codCandidato,
-    ]);
+    try{
+        $curriculo = Curriculo::create([
+            'codCandidato' => $this->candidato->codCandidato,
+        ]);
 
-    DB::table('tbCurriculo')
-    ->where('codCandidato', $candidato->codCandidato)
-    ->update([
-      'descricaoCurriculo' => $request->input('descricaoCurriculo')
-    ]);
+        if ( $curriculo->save() ) {
+            DB::table('tbCurriculo')
+                ->where('codCandidato', $this->candidato->codCandidato)
+                ->update([
+                    'descricaoCurriculo' => $request->input('descricaoCurriculo')
+                ]);
 
-    $this->adicionalController->novoAdicionalCurriculo($request->input('escolaridade'), $curriculo->codCurriculo);
-    $this->adicionalController->novoAdicionalCurriculo($request->input('alfabetizacao'), $curriculo->codCurriculo);
+            $this->adicionalController->novoAdicionalCurriculo($request->input('escolaridade'), $curriculo->codCurriculo);
+            $this->adicionalController->novoAdicionalCurriculo($request->input('alfabetizacao'), $curriculo->codCurriculo);
 
-    foreach ($request->habilidades as $key) {
-      $this->adicionalController->novoAdicionalCurriculo($key, $curriculo->codCurriculo);
+            foreach ($request->habilidades as $key) {
+                $this->adicionalController->novoAdicionalCurriculo($key, $curriculo->codCurriculo);
+            }
+
+            foreach ($request->categorias as $key) {
+                $this->cargoCurriculoController->novoCargoCurriculo($key, $curriculo->codCurriculo);
+            }
+
+            return response()->json('Currículo cadastrado com sucesso');
+        }
+        throw new \Exception('Erro ao criar currículo');
+    } catch (\Exception $e) {
+        return response()->json($e->getMessage(), 503);
     }
-
-    foreach ($request->categorias as $key) {
-      $this->cargoCurriculoController->novoCargoCurriculo($key, $curriculo->codCurriculo);
-    }
-    return redirect('/vagas');
   }
 
   public function viewEditar(){
-    $usuario = Auth::user();
-    $candidato = DB::table('tbCandidato')
-    ->where('codUsuario', $usuario->codUsuario)->first();
-
     $curriculo = DB::table('tbCurriculo')
-    ->where('codCandidato', $candidato->codCandidato)->first();
-
-    $habilidades         = $this->habilidadeController->getHabilidades();
-    $categorias          = $this->categoriaController->getCategorias();
-    $escolaridades       = $this->adicionalController->getAdicionalByNomeTipoAdicional('Escolaridade');
-    $niveisAlfabetizacao = $this->adicionalController->getAdicionalByNomeTipoAdicional('Alfabetização');
+    ->where('codCandidato', $this->candidato->codCandidato)->first();
 
     $dados = [
-      'habilidades'    => $habilidades,
-      'categorias'     => $categorias,
-      'escolaridades'  => $escolaridades,
-      'alfabetizacoes' => $niveisAlfabetizacao,
+      'habilidades'    => $this->habilidadeController->getHabilidades(),
+      'categorias'     => $this->categoriaController->getCategorias(),
+      'escolaridades'  => $this->adicionalController->getAdicionalByNomeTipoAdicional('Escolaridade'),
+      'alfabetizacoes' => $this->adicionalController->getAdicionalByNomeTipoAdicional('Alfabetização'),
       'curriculo'      => $curriculo
     ];
 
     //Habilidades
-    $habilidadesCandidato   = $this->habilidadeController->getHabilidadesByCurriculo($curriculo->codCurriculo);
-    $candidato->habilidades = [];
+    $habilidadesCandidato = $this->habilidadeController->getHabilidadesByCurriculo($curriculo->codCurriculo);
+    $this->candidato->habilidades = [];
     foreach ( $habilidadesCandidato as $habilidade ) {
-      $candidato->habilidades[] = $habilidade->codAdicional;
+      $this->candidato->habilidades[] = $habilidade->codAdicional;
     }
 
-    $categoriasCandidato   = $this->categoriaController->getCategoriasByCurriculo($curriculo->codCurriculo);
-    $candidato->categorias = [];
+    $categoriasCandidato = $this->categoriaController->getCategoriasByCurriculo($curriculo->codCurriculo);
+    $this->candidato->categorias = [];
     foreach ( $categoriasCandidato as $categoria ) {
-      $candidato->categorias[] = $categoria->codCategoria;
+      $this->candidato->categorias[] = $categoria->codCategoria;
     }
 
     $curriculo->escolaridade  = $this->adicionalController->getAdicionalByNomeTipoAndCurriculo('Escolaridade', $curriculo->codCurriculo);
     $curriculo->alfabetizacao = $this->adicionalController->getAdicionalByNomeTipoAndCurriculo('Alfabetização', $curriculo->codCurriculo);
 
     $dados['curriculo'] = $curriculo;
-    $dados['candidato'] = $candidato;
+    $dados['candidato'] = $this->candidato;
 
-    return view('curriculo.curriculo', $dados);
+    return response()->json($dados);
   }
 
   /**
-    * Função para editar o currículo
-    *
-    * @param $request dados do formulário
-    *
-    * @author Vanessa Amaral Marques
-    **/
+  * Função para editar o currículo
+  *
+  * @param Request $request dados do formulário
+  * @return RedirectResponse|Redirector
+  * @author Vanessa Amaral Marques
+  */
   public function atualizarCurriculo(Request $request){
-
-    $usuario   = Auth::user();
-    $candidato = DB::table('tbCandidato')->where('codUsuario', $usuario->codUsuario)->first();
-
     DB::table('tbCurriculo')
-    ->where('codCandidato', $candidato->codCandidato)
+    ->where('codCandidato', $this->candidato->codCandidato)
     ->update([
       'descricaoCurriculo' => $request->input('descricaoCurriculo')
     ]);
 
-    $curriculo = $this->getCurriculobyCandidato($candidato->codCandidato);
+    $curriculo = $this->getCurriculobyCandidato();
 
     //atualizando habilidades
     $codHabilidade = $this->tipoAdicionalController->getTipoAdicionalByNome('Habilidade')->codTipoAdicional;
@@ -181,22 +175,39 @@ class CurriculoController extends Controller
     foreach ($request->categorias as $categoria) {
       $this->cargoCurriculoController->novoCargoCurriculo($categoria, $curriculo->codCurriculo);
     }
-    return redirect("/curriculo/formulario");
+
+    return response()->json();
   }
 
-  public function getCurriculobyCandidato(int $codCandidato){
-    return DB::table('tbCurriculo')
-    ->where('codCandidato', $codCandidato)
-    ->first();
-  }
-
-  public function downloadTexto(){
-    $usuario   = Auth::user();
-    $candidato = DB::table('tbCandidato')
-    ->where('codUsuario', $usuario->codUsuario)->first();
+  /**
+  * Retorna o currículo de um candidato
+  *
+  * @param int|null $codCandidato
+  * @return JsonResponse
+  * @author Vanessa Amaral Marques
+  */
+  public function getCurriculobyCandidato(int $codCandidato = null){
+    if ( ! $codCandidato ) {
+        $codCandidato = $this->candidato->codCandidato;
+    }
 
     $curriculo = DB::table('tbCurriculo')
-    ->where('codCandidato', $candidato->codCandidato)->first();
+    ->where('codCandidato', $codCandidato)
+    ->first();
+
+    return response()->json($curriculo);
+  }
+
+  /**
+  * Download do currículo em modo texto
+  *
+  * @return mixed
+  * @author Michael Andrews
+  * @author Vanessa Amaral Marques
+  */
+  public function downloadTexto(){
+    $curriculo = DB::table('tbCurriculo')
+    ->where('codCandidato', $this->candidato->codCandidato)->first();
 
     return \PDF::loadView('curriculo.curriculo-download-texto', compact('curriculo'))
       ->stream('nome-arquivo-pdf-gerado.pdf');
--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -2,43 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\DB;
 
 class EmpresaController extends Controller
 {
-  /**
-  * Função para cadastrar uma Empresa
-  *
-  * @param array $data array com os dados do formulario
-  * @param int $codUsuario id do usuario recem cadastrado
-  *
-  * @author Michael Andrews
-  **/
-  public function novaEmpresa(array $data, int $codUsuario){
-    $cnpj = $data['cnpj'];
-    $regex = '/[^0-9]/';
-    $cnpj = preg_replace($regex, '', $cnpj);
-
-   Candidato::create([
-     'nomeFantasiaEmpresa' => $data['nomeFantasiaEmpresa'],
-     'razaoSocialEmpresa' => $cnpj,
-     'cnpjEmpresa' => $data['cnpjEmpresa'],
-     'codUsuario' => $codUsuario
-   ]);
-  }
-
-	/**
-    * Função para pegar empresa por codigo
+    /**
+    * Função para cadastrar uma Empresa
     *
-    * @param $codEmpresa codigo da empresa
-    *
+    * @param array $data array com os dados do formulario
+    * @param int $codUsuario id do usuario recem cadastrado
+    * @author Michael Andrews
     * @author Vanessa Amaral Marques
     **/
+    public function novaEmpresa(array $data, int $codUsuario){
+        try {
+            $candidato = Candidato::create([
+                'nomeFantasiaEmpresa' => $data['nomeFantasiaEmpresa'],
+                'razaoSocialEmpresa'  => $this->validaCnpj($data['cnpj']),
+                'cnpjEmpresa'         => $data['cnpjEmpresa'],
+                'codUsuario'          => $codUsuario
+            ]);
+
+            if ( ! $candidato->save() ) {
+                throw new \Exception('Erro ao cadastrar empresa');
+            }
+        } catch (\Exception $e) {
+            response()->json($e->getMessage(), 503);
+        }
+    }
+
+    /**
+     * Retorna uma empresa por codigo
+     *
+     * @param int $codEmpresa codigo da empresa
+     * @return Model|Builder|object|null
+     * @author Vanessa Amaral Marques
+     */
     public function getEmpresaByCod(int $codEmpresa){
-    	return DB::table('tbEmpresa')
+        $empresa = DB::table('tbEmpresa')
         ->select('tbEmpresa.nomeFantasiaEmpresa')
         ->where('tbEmpresa.codEmpresa', '=', $codEmpresa)
         ->first();
+
+        return response()->json($empresa);
+    }
+
+    /**
+     * Valida cnpj
+     *
+     * @param string $cnpj
+     * @return string|string[]|null
+     */
+    private function validaCnpj(string $cnpj){
+        return preg_replace('/[^0-9]/', '', $cnpj);
     }
 }
