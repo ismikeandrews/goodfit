71f61a8cb9e69d332ccb3a0aed9c7f3524e29f04
vanmrqs <vanessa2011164@hotmail.com>
2020-05-11 11:09:11 -0300

    Atualizado controller de candidatura para API

--- app/Http/Controllers/CandidaturaController.php
+++ app/Http/Controllers/CandidaturaController.php
@@ -4,53 +4,61 @@ namespace App\Http\Controllers;
 
 use Auth;
 use App\Candidatura;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\DB;
 
 define('EM_ANDAMENTO', 2);
 
 class CandidaturaController extends Controller
 {
-	/**
-    * Função para cadastrar uma candidatura
-    *
-    * @param $codCategoria codigo da categoria
-    * @param $codCurriculo codigo do curriculo
-    *
-    * @author Vanessa Amaral Marques
-    **/
-    public function novaCandidatura(int $codVaga){
-    	$usuario = Auth::user();
+    private $usuario;
+    private $candidato;
 
-	    $candidato = DB::table('tbCandidato')
-	    ->where('codUsuario', $usuario->codUsuario)->first();
-
-	    Candidatura::create([
-	      'codCandidato' => $candidato->codCandidato,
-	      'codVaga' => $codVaga,
-	      'codStatusCandidatura' => EM_ANDAMENTO
-	    ]);
-
-	    return redirect('/vagas');
+    public function __construct() {
+        $this->usuario   = Auth::user();
+        $this->candidato = DB::table('tbCandidato')->where('codUsuario', $this->usuario->codUsuario)->first();
     }
+
     /**
-      * Função para deletar uma candidatura
-      *
-      * @param $codVaga
-      *
-      * @author Michael Andrews
-      **/
+     * Função para cadastrar uma candidatura
+     *
+     * @param int $codVaga
+     * @return RedirectResponse|Redirector
+     * @author Vanessa Amaral Marques
+    */
+    public function novaCandidatura(int $codVaga){
+	    try{
+            $candidatura = Candidatura::create([
+                'codCandidato'         => $this->candidato->codCandidato,
+                'codVaga'              => $codVaga,
+                'codStatusCandidatura' => EM_ANDAMENTO
+            ]);
 
+            if ( $candidatura->save() ) {
+                return response()->json('Candidatura criada com sucesso', 200);
+            }
+
+            throw new \Exception('Erro ao tentar se candidatar');
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 503);
+        }
+    }
+
+    /**
+     * Função para deletar uma candidatura
+     *
+     * @param $codVaga
+     * @return RedirectResponse|Redirector
+     * @author Michael Andrews
+     * @author Vanessa Amaral Marques
+     **/
     public function excluirCandidatura(int $codVaga){
-      $usuario = Auth::user();
-
-	    $candidato = DB::table('tbCandidato')
-	    ->where('codUsuario', $usuario->codUsuario)->first();
-
-      $candidatura = Candidatura::where('codCandidato', '=', $candidato->codCandidato)
+      $candidatura = Candidatura::where('codCandidato', '=', $this->candidato->codCandidato)
       ->where('codVaga', '=',  $codVaga);
 
-      $candidatura->delete();
-      return redirect("/candidatura");
+      if ( ! $candidatura->delete() ) {
+          return response()->json('Erro ao tentar remover candidatura', 503);
+      }
     }
 }
