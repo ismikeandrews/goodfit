303b61ad46edc0860a2fb5e4b5324a857faeda50
vanmrqs <vanessa2011164@hotmail.com>
2020-05-11 11:20:50 -0300

    Atualizados controllers de cargo e categoria para API

--- app/Http/Controllers/CargoCurriculoController.php
+++ app/Http/Controllers/CargoCurriculoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\CargoCurriculo;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -10,24 +11,44 @@ use Illuminate\Support\Facades\Validator;
 class CargoCurriculoController extends Controller
 {
     /**
-    * Função para cadastro de cargo em um currículo
-    *
-    * @param $codCategoria codigo da categoria
-    * @param $codCurriculo codigo do curriculo
-    *
-    * @author Vanessa Amaral Marques
-    **/
+     * Função para cadastro de cargo em um currículo
+     *
+     * @param int $codCategoria codigo da categoria
+     * @param int $codCurriculo codigo do curriculo
+     * @return JsonResponse
+     * @author Vanessa Amaral Marques
+     */
     public function novoCargoCurriculo(int $codCategoria, int $codCurriculo){
-    	CargoCurriculo::create([
-	    	'codCategoria' => $codCategoria,
-	        'codCurriculo' => $codCurriculo
-	    ]);
+    	try{
+    	    $cargo = CargoCurriculo::create([
+                'codCategoria' => $codCategoria,
+                'codCurriculo' => $codCurriculo
+            ]);
+
+    	    if ( $cargo->save() ) {
+    	        return response()->json('Cargo criado com sucesso', 200);
+            }
+
+    	    throw new \Exception('Erro ao tentar criar cargo em currículo');
+        } catch (\Exception $e) {
+    	    return response()->json($e->getMessage(), 503);
+        }
     }
 
+    /**
+     * Remove um cargo de um currículo
+     *
+     * @param int $codCurriculo
+     * @return JsonResponse
+     * @author Vanessa Amaral Marques
+     */
     public function removerCargoByCurriculo(int $codCurriculo){
-        return DB::table('tbCargoCurriculo')
+        $cargo =  DB::table('tbCargoCurriculo')
         ->join('tbCategoria', 'tbCargoCurriculo.codCategoria', '=', 'tbCategoria.codCategoria')
-        ->where('tbCargoCurriculo.codCurriculo', '=', $codCurriculo)
-        ->delete();
+        ->where('tbCargoCurriculo.codCurriculo', '=', $codCurriculo);
+
+        if ( ! $cargo->delete() ) {
+            return response()->json('Erro ao excluir cargo', 503);
+        }
     }
 }
--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,51 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class CategoriaController extends Controller
 {
+    /**
+     * Cadastra uma nova categoria
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     * @author Vanessa Amaral Marques
+     */
     public function cadastroCategoria(Request $request){
-
     	$this->validate($request, [
     		'nomeCategoria' => 'required|max:100|string|unique:tbCategoria,nomeCategoria'
     	]);
 
-    	$categoria = Categoria::create([
-    		'nomeCategoria' => $request->input('nomeCategoria'),
-    	]);
+    	try {
+            $categoria = Categoria::create([
+                'nomeCategoria' => $request->input('nomeCategoria'),
+            ]);
 
-    	return view('cadastroCategoria')->with('ok', $categoria->save());
+            if ( $categoria->save() ) {
+                return response()->json('Categoria cadastrada com sucesso');
+            }
+
+            throw new \Exception('Erro ao tentar cadastrar categoria');
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 503);
+        }
     }
 
     /**
-    * Função para pegar todos as categorias
-    * 
-    *
-    * @author Vanessa Amaral Marques
-    **/
+     * Retorna as categorias cadastradas
+     *
+     * @return JsonResponse Collection
+     * @author Vanessa Amaral Marques
+     */
     public function getCategorias(){
-    	return DB::table('tbCategoria')
+    	$categorias = DB::table('tbCategoria')
 	      ->select('tbCategoria.codCategoria', 'tbCategoria.nomeCategoria', 'tbCategoria.imagemCategoria')
 	      ->orderBy('tbCategoria.nomeCategoria')
 	      ->get();
+
+    	return response()->json($categorias);
     }
 
     /**
-    * Função para pegar todos as categorias de um curriculo
-    * 
-    * @param $codCurriculo codigo do curriculo
-    *
-    * @author Vanessa Amaral Marques
-    **/
+     * Retorna todos as categorias de um curriculo
+     *
+     * @param int $codCurriculo codigo do curriculo
+     * @return JsonResponse Collection
+     * @author Vanessa Amaral Marques
+     */
     public function getCategoriasByCurriculo(int $codCurriculo){
-    	return DB::table('tbCategoria')
+    	$categorias = DB::table('tbCategoria')
 	      ->select('tbCategoria.codCategoria', 'tbCategoria.nomeCategoria', 'tbCategoria.imagemCategoria')
 	      ->join('tbCargoCurriculo', 'tbCategoria.codCategoria', '=', 'tbCargoCurriculo.codCategoria')
 	      ->where('tbCargoCurriculo.codCurriculo', $codCurriculo)
 	      ->orderBy('tbCategoria.nomeCategoria')
 	      ->get();
+
+    	return response()->json($categorias);
     }
 }
